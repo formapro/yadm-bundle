e7fac04bde96ad6bdd9b2d55eca53f7563155a33
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-03-20 13:53:55 +0200

    Print done at the end of cmd

--- Command/LoadDataFixturesYadmCommand.php
+++ Command/LoadDataFixturesYadmCommand.php
@@ -91,6 +91,8 @@ class LoadDataFixturesYadmCommand extends Command implements ContainerAwareInter
                 realpath($dumpFile)
             ));
         }
+
+        $output->writeln('Done');
     }
 
     /**
